267256e05ba3735aa9312d7ca15aa44bd5412e45
Nagesh <nageshtirumani6@gmail.com>
2025-02-24 12:59:36 +0530

    I create incress salary class

---
+++ IncrimentSlaryOfEmployee.java
@@ -0,0 +1,15 @@
+public class IncrimentSlaryOfEmployee {
+    static String Name="Suresh";
+    static int salary=50000;
+    static String company="TCS";
+    static String place="Hyderabad";
+    static int noticePeriod=30;
+    static void incresSlary(){
+        System.out.println(Name+" "+(salary+50000)+" "+company+" "+place+" "+noticePeriod);
+    }
+    public static void main(String [] args){
+        incresSlary();
+    }
+
+
+}
