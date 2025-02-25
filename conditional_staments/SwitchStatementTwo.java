199cbb51b8d1f5b094ab76fd1bebed526faba18d
Nagesh <nageshtirumani6@gmail.com>
2025-02-25 17:20:48 +0530

    switch code

---
+++ conditional_staments/SwitchStatementTwo.java
@@ -0,0 +1,42 @@
+package conditional_staments;
+
+public class SwitchStatementTwo {
+    public static void main(String [] args){
+        int number=10;
+        switch(number){
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            case 4:
+                System.out.println("four");
+                break;
+            case 5:
+                System.out.println("five");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 8:
+                System.out.println("eight");
+                break;
+            case 9:
+                System.out.println("nine");
+                break;
+            case 10:
+                System.out.println("Ten");
+                break;
+            default:
+                System.out.println("Greather than Ten");
+
+        }
+    }
+}
